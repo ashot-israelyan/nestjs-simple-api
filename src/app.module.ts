f6d3a87c9d4ad7c4504bedf44189018b26ece3b4
Ashot Israelyan <ashot@picsart.com>
2023-07-07 11:47:34 +0400

    Cookie key moved to env. Sample env added

--- src/app.module.ts
+++ src/app.module.ts
@@ -43,11 +43,12 @@ const cookieSession = require('cookie-session');
   ],
 })
 export class AppModule {
+  constructor(private configService: ConfigService) {}
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(
         cookieSession({
-          keys: ['sSbzjI@U'],
+          keys: [this.configService.get('COOKIE_KEY')],
         }),
       )
       .forRoutes('*');
