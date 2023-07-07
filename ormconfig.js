951552bac0fb1bc84789aa0fec3e901d2d0c7739
Ashot Israelyan <ashot@picsart.com>
2023-07-07 16:32:55 +0400

    DB migration added

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,29 @@
-module.exports = {
-  type: 'sqlite',
-  database: 'db.sqlite',
-  entities: ['**/*.js'],
+let dbConfig = {
   synchronize: false,
+  migrations: ['migrations/*.js'],
+  cli: {
+    migrationsDir: 'migrations',
+  },
 };
+
+switch (process.env.NODE_ENV) {
+  case 'development':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: ['**/*.entity.js'],
+    });
+    break;
+  case 'test':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'test.sqlite',
+      entities: ['**/*.entity.ts'],
+    });
+  case 'production':
+    break;
+  default:
+    throw new Error('Unknown environment');
+}
+
+module.exports = dbConfig;
--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -6,10 +6,10 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { plainToInstance } from 'class-transformer';
+import { plainToClass } from 'class-transformer';
 
 interface ClassConstructor {
-  new (...args: any[]): object;
+  new (...args: any[]): Record<any, any>;
 }
 
 export function Serialize(dto: ClassConstructor) {
@@ -19,13 +19,10 @@ export function Serialize(dto: ClassConstructor) {
 export class SerializeInterceptor implements NestInterceptor {
   constructor(private dto: ClassConstructor) {}
 
-  intercept(
-    context: ExecutionContext,
-    handler: CallHandler<any>,
-  ): Observable<any> {
+  intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
     return handler.handle().pipe(
       map((data: any) => {
-        return plainToInstance(this.dto, data, {
+        return plainToClass(this.dto, data, {
           excludeExtraneousValues: true,
         });
       }),
